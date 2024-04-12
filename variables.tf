3fa46fb7e33f7a539aa69ff503e618d1e13dd820
theppasiva <theppasiva@gmail.com>
2024-04-12 20:18:33 +0530

    terraform-aws-vpc

--- variables.tf
+++ variables.tf
@@ -31,26 +31,68 @@ variable "igw_tags" {
     default = {}
 }
 
-variable "public_subnet_cidr" {
-    type = list 
-    validation {
-        condition = length(var.public_subnet_cidr) == 2
-        error_message = "please give 2 public valid subnet CIDR"
-    }
+variable "public_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.public_subnets_cidr) == 2
+    error_message = "Please give 2 public valid subnet CIDR"
+  }
 }
 
-variable "public_subnet_tags" {
+variable "public_subnets_tags" {
+  default = {}
+}
+
+variable "private_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.private_subnets_cidr) == 2
+    error_message = "Please give 2 private valid subnet CIDR"
+  }
+}
+
+variable "private_subnets_tags" {
+  default = {}
+}
+
+variable "database_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.database_subnets_cidr) == 2
+    error_message = "Please give 2 database valid subnet CIDR"
+  }
+}
+
+variable "database_subnets_tags" {
+  default = {}
+}
+
+variable "nat_gateway_tags" {
     default = {}
 }
 
-variable "private_subnet_cidr" {
-    type = list 
-    validation {
-        condition = length(var.private_subnet_cidr) == 2
-        error_message = "please give 2 private valid subnet CIDR"
-    }
+variable "public_route_table_tags" {
+    default = {}
 }
 
-variable "private_subnet_tags" {
+variable "private_route_table_tags" {
     default = {}
+}
+
+variable "database_route_table_tags" {
+    default = {}
+}
+
+variable "is_peering_required" {
+  type = bool
+  default = false
+}
+
+variable "acceptor_vpc_id" {
+  type = string
+  default = ""
+}
+
+variable "vpc_peering_tags" {
+  default = {}
 }
